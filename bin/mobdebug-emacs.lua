3b668aaef6dbd56ba219b4d496b3631ed5e3d93e
Jacob Salzberg <jssalzbe@ncsu.edu>
2020-06-15 11:58:45 -0400

    Add breakpoint echoing

--- bin/mobdebug-emacs.lua
+++ bin/mobdebug-emacs.lua
@@ -15,26 +15,35 @@ mobdebug.handle("load " .. arg[2], client)
 
 print("Lua Remote Debugger")
 print("Run the program you wish to debug")
+print("Paused at file " .. arg[2] .. " line 1")
 
-client:send("STEP\n")
-client:receive()
+-- client:send("STEP\n")
+-- client:receive()
 
-local breakpoint = client:receive()
-local _, _, file, line = string.find(breakpoint, "^202 Paused%s+(.-)%s+(%d+)%s*$")
-if file and line then
-   print("Paused at file " .. file )
-   print("Type 'help' for commands")
-else
-   local _, _, size = string.find(breakpoint, "^401 Error in Execution (%d+)%s*$")
-   if size then
-      print("Error in remote application: ")
-      print(client:receive(size))
-   end
-end
+-- local breakpoint = client:receive()
+-- local _, _, file, line = string.find(breakpoint, "^202 Paused%s+(.-)%s+(%d+)%s*$")
+-- if file and line then
+--    print("Paused at file " .. file )
+--    print("Type 'help' for commands")
+-- else
+--    local _, _, size = string.find(breakpoint, "^401 Error in Execution (%d+)%s*$")
+--    if size then
+--       print("Error in remote application: ")
+--       print(client:receive(size))
+--    end
+-- end
 
 while true do
+   local breakpoint_number = 1
    io.write("> ")
-   local file, line, err = mobdebug.handle(io.read("*line"), client)
+   local line_to_handle = io.read("*line")
+   local file, line, err = mobdebug.handle(line_to_handle, client)
+   local beg_setb, end_setb = string.find(line_to_handle, "setb ")
+   if beg_setb then
+      local _, _, _, file, line = string.find(line_to_handle, "^([a-z]+)%s+(.-)%s+(%d+)%s*$")
+      print(string.format("Breakpoint " .. breakpoint_number .. " set at file " .. file .. " line " .. line))
+      breakpoint_number = breakpoint_number + 1
+   end
    if not file and err == false then break end -- completed debugging
 end
 
